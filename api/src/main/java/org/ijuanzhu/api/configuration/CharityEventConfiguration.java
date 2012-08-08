7747bbde699a3030fab4d3cfaeaf656718274191
cyang <chen.yang@bazaarvoice.com>
2012-08-07 22:13:52 -0500

    Stub

---
+++ api/src/main/java/org/ijuanzhu/api/configuration/CharityEventConfiguration.java
@@ -0,0 +1,6 @@
+package org.ijuanzhu.api.configuration;
+
+import com.yammer.dropwizard.config.Configuration;
+
+public class CharityEventConfiguration extends Configuration {
+}
---
+++ api/src/main/java/org/ijuanzhu/api/service/CharityEventService.java
@@ -0,0 +1,14 @@
+package org.ijuanzhu.api.service;
+
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Environment;
+import org.ijuanzhu.api.configuration.CharityEventConfiguration;
+
+
+public class CharityEventService extends Service<CharityEventConfiguration> {
+
+    @Override
+    protected void initialize(CharityEventConfiguration charityEventConfiguration, Environment environment) throws Exception {
+
+    }
+}
